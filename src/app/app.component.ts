c9fd932001bd39b218aef75604bbd7cd934efb05
Gabriel Abrão <114771992+ggabrao@users.noreply.github.com>
2024-08-18 17:39:50 -0300

    Dynamic Templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,9 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'essentials';
+  title = 'initial title';
+
+  updateTitle() {
+    this.title = 'updated title';
+  }
 }
